5336e889ffbf5bb19de6e07a384a8a2d643cf236
Maurice CGP Peters <MCGPPeters@users.noreply.github.com>
2025-06-20 20:19:03 +0200

    Update project files to target .NET 9.0 and modify launch configurations for Firefox and Chrome

--- Abies.Conduit/Navigation.cs
+++ Abies.Conduit/Navigation.cs
@@ -18,24 +18,24 @@ public static class Navigation
         var userLinks = model.CurrentUser == null
             ? new Node[]
             {
-                NavLink("/login", "Sign in", model.CurrentRoute is Route.Login),
-                NavLink("/register", "Sign up", model.CurrentRoute is Route.Register)
+                NavLink("/login", "Sign in", model.CurrentRoute is Routing.Route.Login),
+                NavLink("/register", "Sign up", model.CurrentRoute is Routing.Route.Register)
             }
             : new Node[]
             {
-                NavLink("/editor", "New Article", model.CurrentRoute is Route.NewArticle),
-                NavLink("/settings", "Settings", model.CurrentRoute is Route.Settings),
+                NavLink("/editor", "New Article", model.CurrentRoute is Abies.Conduit.Routing.Route.NewArticle),
+                NavLink("/settings", "Settings", model.CurrentRoute is Routing.Route.Settings),
                 NavLink($"/profile/{model.CurrentUser!.Username.Value}",
                         model.CurrentUser.Username.Value,
-                        model.CurrentRoute is Route.Profile p && p.UserName.Value == model.CurrentUser.Username.Value ||
-                        model.CurrentRoute is Route.ProfileFavorites pf && pf.UserName.Value == model.CurrentUser.Username.Value)
+                        model.CurrentRoute is Routing.Route.Profile p && p.UserName.Value == model.CurrentUser.Username.Value ||
+                        model.CurrentRoute is Routing.Route.ProfileFavorites pf && pf.UserName.Value == model.CurrentUser.Username.Value)
             };
 
         return nav([class_("navbar navbar-light")], [
             div([class_("container")], [
                 a([class_("navbar-brand"), href("/")], [text("conduit")]),
                 ul([class_("nav navbar-nav pull-xs-right")], [
-                    NavLink("/", "Home", model.CurrentRoute is Route.Home),
+                    NavLink("/", "Home", model.CurrentRoute is Routing.Route.Home),
                     ..userLinks
                 ])
             ])
