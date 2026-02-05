1bb3ea952f1197a8565ec106e39704fb232067b4
github-action-benchmark <github@users.noreply.github.com>
2026-02-05 18:17:10 +0000

    add Virtual DOM Benchmarks (benchmarkdotnet) benchmark result for bd568e31b00b62373fed10559b6a52906a7bc3bc

---
+++ dev/bench/data.js
@@ -0,0 +1,72 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1770315430878,
+  "repoUrl": "https://github.com/Picea/Abies",
+  "entries": {
+    "Virtual DOM Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "Maurice CGP Peters",
+            "username": "MCGPPeters",
+            "email": "MCGPPeters@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "bd568e31b00b62373fed10559b6a52906a7bc3bc",
+          "message": "fix: Use string comparison for gh-pages existence check (#29)",
+          "timestamp": "2026-02-05T17:47:10Z",
+          "url": "https://github.com/Picea/Abies/commit/bd568e31b00b62373fed10559b6a52906a7bc3bc"
+        },
+        "date": 1770315430138,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Abies.Benchmarks.DomDiffingBenchmarks.SmallDomDiff",
+            "value": 690.0484898249308,
+            "unit": "ns",
+            "range": "± 2.1269517574730497"
+          },
+          {
+            "name": "Abies.Benchmarks.DomDiffingBenchmarks.MediumDomDiff",
+            "value": 827.6673535664876,
+            "unit": "ns",
+            "range": "± 8.439520430665128"
+          },
+          {
+            "name": "Abies.Benchmarks.DomDiffingBenchmarks.LargeDomDiff",
+            "value": 713.4371990839641,
+            "unit": "ns",
+            "range": "± 3.208940245449105"
+          },
+          {
+            "name": "Abies.Benchmarks.DomDiffingBenchmarks.AttributeOnlyDiff",
+            "value": 712.5139598846436,
+            "unit": "ns",
+            "range": "± 2.711803127107198"
+          },
+          {
+            "name": "Abies.Benchmarks.DomDiffingBenchmarks.TextOnlyDiff",
+            "value": 846.2599952697753,
+            "unit": "ns",
+            "range": "± 8.453522590224914"
+          },
+          {
+            "name": "Abies.Benchmarks.DomDiffingBenchmarks.NodeAdditionDiff",
+            "value": 853.6884147644043,
+            "unit": "ns",
+            "range": "± 8.326568448249187"
+          },
+          {
+            "name": "Abies.Benchmarks.DomDiffingBenchmarks.NodeRemovalDiff",
+            "value": 871.9484014511108,
+            "unit": "ns",
+            "range": "± 9.215764583280613"
+          }
+        ]
+      }
+    ]
+  }
+}
