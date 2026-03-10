35fb2f7e67260ec1b278534d3303b7a4ffa37237
Maurice CGP Peters <MCGPPeters@users.noreply.github.com>
2026-03-10 12:54:48 +0100

    refactor: port Conduit.AppHost to Picea.Abies namespace

---
+++ Picea.Abies.Conduit.AppHost/Program.cs
@@ -0,0 +1,13 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+// Aspire Dashboard can be launched by the Aspire CLI/workload; desired port can be set via env var
+builder.Configuration["DOTNET_DASHBOARD_PORT"] = "18888";
+
+// Backend API
+var api = builder.AddProject("conduit-api", "../Picea.Abies.Conduit.Api/Picea.Abies.Conduit.Api.csproj");
+
+// Frontend app
+builder.AddProject("conduit-app", "../Picea.Abies.Conduit/Picea.Abies.Conduit.csproj")
+       .WithReference(api);
+
+builder.Build().Run();
