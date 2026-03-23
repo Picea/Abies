e765bc64533b84b490fde2342d2e469ef30462eb
Maurice Cornelius Gerardus Petrus Peters <me@mauricepeters.dev>
2026-03-23 20:20:46 +0100

    test: Add failing tests for issue #160 debugger (adapter contract, mount point, release-strip)

---
+++ Picea.Abies.Browser.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Copyright (c) 2024 Abies Contributors. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+global using TUnit.Assertions;
+global using System.Reflection;
